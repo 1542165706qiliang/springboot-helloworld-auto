95ccbe0ff9a376b60660bc7f3f2d6883abbd22fc
qi <18406459159@163.com>
2020-06-04 08:56:42 +0800

    IDEA测试Git的使用

---
+++ src/main/java/com/atguigu/springboot/SpringBootHelloWorldAutoApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootHelloWorldAutoApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext act =  SpringApplication.run(SpringBootHelloWorldAutoApplication.class, args);
+
+}
+
+}
