bac960ca0515fb8818f5f1aefa65bc3deaa28668
qdl <18406459159@163.com>
2020-10-24 19:21:14 +0800

    去除多余导入

--- src/main/java/com/atguigu/springboot/SpringBootHelloWorldAutoApplication.java
+++ src/main/java/com/atguigu/springboot/SpringBootHelloWorldAutoApplication.java
@@ -2,7 +2,6 @@ package com.atguigu.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootHelloWorldAutoApplication {
